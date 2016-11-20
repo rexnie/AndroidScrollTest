9b394e6d7fe1eceecfb5bef93636522729dfd773
niedaocai <niedaocai@snda.com>
2016-11-21 00:16:29 +0800

    使用ViewDragHelper实现View的移动

---
+++ app/src/main/java/com/example/android/androidscrolltest/ViewDragHelperActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.androidscrolltest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewDragHelperActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_drag_helper);
+    }
+}
